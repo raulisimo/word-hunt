e9a0b2efbcb2e69a5d554b0192fac4862e6641ae
Raul <raulraul.sp@gmail.com>
2021-09-05 20:06:46 +0200

    Added support to PWA

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 ReactDOM.render(
-	<App>
+	<React.StrictMode>
 		<App />
-	</App>,
+	</React.StrictMode>,
 	document.getElementById('root')
 )
+
+serviceWorkerRegistration.register()
